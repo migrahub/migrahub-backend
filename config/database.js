f0618f8d9857d1e3fbceadf573e6f0210b550c0b
Pedro Bini <dev.pedrobini@gmail.com>
2020-07-14 23:45:15 -0300

    fix: development database config

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'migrahub'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
       },
